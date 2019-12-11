90a86546eccafb88728febd74143c39057158bd4
Nikolay0951 <egnicholas333@gmail.com>
2019-12-11 23:30:59 +0300

    добавлен запрос новых постов

--- app.js
+++ app.js
@@ -55,4 +55,44 @@ server.delete('/posts/:id/likes', (req, res) => {
     res.send(`${posts[index].likes}`);
 });
 
+
+server.get('/posts/seenPosts/:lastSeenId', (req, res) => {
+    const lastSeenId = +req.params['lastSeenId']
+    let lastPosts;
+    if (lastSeenId === 0) {
+        lastPosts = posts.length < 5 ? posts : posts.slice(posts.length - 5);
+    } else {
+        const sortedPosts = posts.filter(post => post.id < lastSeenId);
+        lastPosts = sortedPosts.length < 5 ? sortedPosts : sortedPosts.slice(sortedPosts.length - 5);    
+    }
+    res.send(lastPosts);
+});
+
+server.get('/posts/newPosts/:firstSeenId', (req, res) => {
+    const firstSeenId = +req.params['firstSeenId'];
+    const lastPosts = posts.filter(post => post.id > firstSeenId);
+    res.send(lastPosts);
+})
+
+server.get('/posts/poll-seenPosts/:fifthPostId', (req, res) => {
+    const fifthPostId = +req.params['fifthPostId'];
+    if (fifthPostId === posts[0].id) {
+        res.send(true);
+        return;
+    }
+    res.send(false);    
+})
+
+server.get('/posts/poll/:firstSeenId', (req, res) => {
+    const newPostId = +req.params['firstSeenId'];
+    if (posts.length === 0) {
+        res.send(false);
+        return;
+    } else if (newPostId < posts[posts.length - 1].id) {
+        res.send(true);
+        return;
+    }
+    res.send(false);
+})
+
 server.listen(process.env.PORT || '9999');
